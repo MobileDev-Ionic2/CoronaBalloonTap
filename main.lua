5d9111032edccbcfbd20e3e1c6aa83ca20c0aede
ebbevang <ev@evang.dk>
2017-04-19 10:33:47 +0200

    add all files

---
+++ main.lua
@@ -0,0 +1,52 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+local tapCount = 0
+
+local background = display.newImageRect( "background.png", 360, 570 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+local tapText = display.newText( tapCount, display.contentCenterX, 20, native.systemFont, 40 )
+tapText:setFillColor( 0, 0, 0 )
+
+local platform = display.newImageRect( "platform.png", 300, 50 )
+platform.x = display.contentCenterX
+platform.y = display.contentHeight-25
+
+local balloon = display.newImageRect( "balloon.png", 112, 112 )
+balloon.x = display.contentCenterX
+balloon.y = display.contentCenterY
+balloon.alpha = 0.8
+
+local physics = require( "physics" )
+physics.start()
+
+physics.addBody( platform, "static" )
+physics.addBody( balloon, "dynamic", { radius=50, bounce=0.3 } )
+
+local function on_frame( event )
+    if tapCount > 0 then
+        local tapText2 = display.newText( "!", display.contentCenterX, 50, native.systemFont, 40 )    
+        tapText2:setFillColor( 0, 0, 0 )
+    end
+end
+
+local function pushBalloon()
+	balloon:applyLinearImpulse( 0, -0.75, balloon.x, balloon.y )
+	tapCount = tapCount + 1
+	tapText.text = tapCount
+end
+
+local function onGlobalCollision( event )
+    tapCount = 0
+    tapText.text = tapCount
+end
+
+balloon.collision = onGlobalCollision
+balloon:addEventListener( "collision" )
+balloon:addEventListener( "tap", pushBalloon )
+Runtime:addEventListener( "enterFrame", on_frame )
